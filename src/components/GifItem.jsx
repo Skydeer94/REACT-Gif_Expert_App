79a3a7772645ee37d804df71d68e10333e56dac4
Steven Angel <stevenangel.ing@gmail.com>
2023-03-30 14:10:48 -0500

    Test realizados

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from 'prop-types';
 
 export const GifItem = ( {title, url}) => {
   
@@ -9,4 +9,10 @@ export const GifItem = ( {title, url}) => {
             <p>{ title }</p>    
         </div>
   )
+}
+
+
+GifItem.PropTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
 }
